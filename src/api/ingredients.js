d764bd21aa021e74bee36da861889209db176cb6
Yuriy Tokarev <yuriytok@gmail.com>
2022-10-30 11:31:06 +0400

    Add test data for frontend

---
+++ src/api/ingredients.js
@@ -0,0 +1,11 @@
+import testData from "../utils/data.js";
+
+/**
+ * Get ingredients data
+ * @returns array 
+ */
+function getIngredientsService() {
+    return testData;
+}
+
+export default getIngredientsService;
