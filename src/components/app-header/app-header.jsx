31898acb3e7f6b28db931fe47e08c7a8b346bfef
Yuriy Tokarev <yuriytok@gmail.com>
2022-10-30 12:14:44 +0400

    Base structure

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './app-header.module.css';
+
+class AppHeader extends React.Component {
+    render() {
+        return (
+            <header>
+                Header
+            </header>
+        );
+    }
+}
+
+export default AppHeader;
