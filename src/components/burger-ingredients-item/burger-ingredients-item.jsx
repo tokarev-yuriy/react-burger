d525a2dafa9e2ce78930185a3285c1dec6968c64
Yuriy Tokarev <yuriytok@gmail.com>
2022-11-05 19:13:21 +0400

    Refactor class components to func components 2

--- src/components/burger-ingredients-item/burger-ingredients-item.jsx
+++ src/components/burger-ingredients-item/burger-ingredients-item.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import styles from './burger-ingredients-item.module.css';
-import {CurrencyIcon, Counter} from '@ya.praktikum/react-developer-burger-ui-components';
+import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 import PropTypes from 'prop-types';
 import { ingredientPropTypes } from '../../utils/prop-type';
 
-class BurgerIngredientsItem extends React.Component {
-    render() {
-        return (
-            <div key={this.props.item._id} className={styles.item}>
-                <Counter count={this.props.count} />
-                <img src={this.props.item.image} alt={this.props.item.name} className={styles.item_img} />
-                <p className={styles.item_price}>
-                    {this.props.item.price}
-                    <CurrencyIcon />
-                </p>
-                <p className={styles.item_name}>{this.props.item.name}</p>
-            </div>
-        );
-    }
+function BurgerIngredientsItem(props) {
+    return (
+        <div key={props.item._id} className={styles.item}>
+            <Counter count={props.count} />
+            <img src={props.item.image} alt={props.item.name} className={styles.item_img} />
+            <p className={styles.item_price}>
+                {props.item.price}
+                <CurrencyIcon />
+            </p>
+            <p className={styles.item_name}>{props.item.name}</p>
+        </div>
+    );
 }
 
 BurgerIngredientsItem.propTypes = {
@@ -25,4 +23,4 @@ BurgerIngredientsItem.propTypes = {
     item: ingredientPropTypes.isRequired,
 };
 
-export default BurgerIngredientsItem;
+export { BurgerIngredientsItem };
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,78 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './burger-ingredients.module.css';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
-import BurgerIngredientsItem from '../burger-ingredients-item/burger-ingredients-item';
+import { BurgerIngredientsItem } from '../burger-ingredients-item/burger-ingredients-item';
 import PropTypes from 'prop-types';
 import { ingredientPropTypes } from '../../utils/prop-type';
+import { useCallback } from 'react';
 
-class BurgerIngredients extends React.Component {
-    state = {
-        tab: 'bun'
-    };
+function BurgerIngredients(props) {
+
+    const [activeTab, setActiveTab] = useState('bun');
 
     /**
      * меняем активный tab
      * @param string val 
      */
-    setTab = (val) => {
-        const tab = document.getElementById('ingredients-tab-'  + val);
-        tab.scrollIntoView();
-        this.setState({
-            ...this.state,
-            tab: val
-        })
-    }
+    const changeTab = useCallback((val) => {
+        setActiveTab(val);
+        const tabElement = document.getElementById('ingredients-tab-'  + val);
+        if (tabElement) {
+            tabElement.scrollIntoView();
+        }
+    }, []);
 
-    render() {
-        const types = [
-            {
-                'code': 'bun',
-                'title': 'Булки',
-                'items': this.props.items.filter((data) => data.type === 'bun'),
-            },
-            {
-                'code': 'sauce',
-                'title': 'Соусы',
-                'items': this.props.items.filter((data) => data.type === 'sauce'),
-            },
-            {
-                'code': 'main',
-                'title': 'Начинки',
-                'items': this.props.items.filter((data) => data.type === 'main'),
-            },
-        ];
+    const types = [
+        {
+            'code': 'bun',
+            'title': 'Булки',
+            'items': props.items.filter((data) => data.type === 'bun'),
+        },
+        {
+            'code': 'sauce',
+            'title': 'Соусы',
+            'items': props.items.filter((data) => data.type === 'sauce'),
+        },
+        {
+            'code': 'main',
+            'title': 'Начинки',
+            'items': props.items.filter((data) => data.type === 'main'),
+        },
+    ];
 
-        return (
-            <section className={styles.section}>
-                <h1 className={styles.section_title}>Соберите бургер</h1>
-                
-                <div className={styles.section_tabs}>
-                    {types.map((type)=>(
-                        <Tab value={type.code} active={this.state.tab === type.code} onClick={this.setTab} key={type.code}>
-                            {type.title}
-                        </Tab>
-                    ))}
-                </div>
-                
-                <div className={styles.scrollable}>
-                    {types.map((type)=>(
-                        <React.Fragment key={type.code}>
-                            <h2 className={styles.section_items_title} id={'ingredients-tab-' + type.code}>{type.title}</h2>
-                            <div className={styles.ingredients}>
-                                {type.items.map((ingredient) => (
-                                    <BurgerIngredientsItem item={ingredient} key={ingredient._id} count={1} />
-                                ))}
-                            </div>
-                        </React.Fragment>
-                    ))}
-                </div>
-            </section>
-        );
-    }
+    return (
+        <section className={styles.section}>
+            <h1 className={styles.section_title}>Соберите бургер</h1>
+            
+            <div className={styles.section_tabs}>
+                {types.map((type)=>(
+                    <Tab value={type.code} active={type.code === activeTab} onClick={changeTab} key={type.code}>
+                        {type.title}
+                    </Tab>
+                ))}
+            </div>
+            
+            <div className={styles.scrollable}>
+                {types.map((type)=>(
+                    <React.Fragment key={type.code}>
+                        <h2 className={styles.section_items_title} id={'ingredients-tab-' + type.code}>{type.title}</h2>
+                        <div className={styles.ingredients}>
+                            {type.items.map((ingredient) => (
+                                <BurgerIngredientsItem item={ingredient} key={ingredient._id} count={1} />
+                            ))}
+                        </div>
+                    </React.Fragment>
+                ))}
+            </div>
+        </section>
+    );
 }
 
 BurgerIngredients.propTypes = {
     items: PropTypes.arrayOf(ingredientPropTypes.isRequired).isRequired,
 }; 
 
-export default BurgerIngredients;
+export { BurgerIngredients };
