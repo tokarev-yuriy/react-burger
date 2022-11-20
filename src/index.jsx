e4cc4797bf8d724c8055f9f31b53342029ee5861
Yuriy Tokarev <yuriytok@gmail.com>
2022-11-21 00:58:48 +0400

    Move store init to services/index

--- src/index.jsx
+++ src/index.jsx
@@ -3,19 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './components/app/app.jsx';
 import '@ya.praktikum/react-developer-burger-ui-components';
-import { compose, createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { rootReducer } from './services/reducers';
-import thunk from 'redux-thunk';
+import { store } from './services/index'
 
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-
-const store = createStore(rootReducer, enhancer);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
