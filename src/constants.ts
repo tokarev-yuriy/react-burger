2d0a30fa4ddc721222736fa6753d769f68d5bb5c
Yuriy Tokarev <yuriytok@gmail.com>
2023-01-29 17:18:46 +0100

    Fix

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 // API endpoint
 export const BURGER_API_URL: string = "https://norma.nomoreparties.space/api";
-export const APP_BASE_URL: string = "/reactor-burger/";
+export const APP_BASE_URL: string = "/react-burger/";
